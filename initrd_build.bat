e424dab3ee1fc7e87db519aa5cadc7cf63e033c5
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2017-03-06 22:37:32 -0800

    Resolved issues with threading system. Added Station Access Control program, currently linked to kernel.

--- initrd_build.bat
+++ initrd_build.bat
@@ -1,3 +1,3 @@
 xcopy /y initrd_bootstrap\initrd_bootstrap.bin initrd_filesystem\terminal
-xcopy /y SHELL\SHELL.bin initrd_filesystem\shell
+xcopy /y SHELL\SHELL.bin initrd_filesystem\txtio
 darkOsRsfs.exe rsfs initrd .\initrd_filesystem
