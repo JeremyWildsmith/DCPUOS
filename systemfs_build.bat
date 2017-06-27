f9973f7e1b94bc467447684b74497d9802a517c9
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2017-06-27 00:55:39 -0700

    Added build script to create FAT12 filesystem.

--- systemfs_build.bat
+++ systemfs_build.bat
@@ -1,1 +1,12 @@
-darkOsRsfs.exe lafs systemFilesystem .\system_filesystem
+del filesystem.vhd
+del filesystem.img
+copy template.vhd .\filesystem.vhd
+((echo select vdisk file=C:\Users\Jeremy\Desktop\DCPUOS-master\filesystem.vhd) & (echo attach vdisk)) | diskpart
+
+for /f %%D in ('wmic volume get DriveLetter^, Label ^| find "DCPUOSFS"') do set myDrive=%%D
+
+xcopy /s C:\Users\Jeremy\Desktop\DCPUOS-master\system_filesystem\*.* %myDrive%\
+
+((echo select vdisk file=C:\Users\Jeremy\Desktop\DCPUOS-master\filesystem.vhd) & (echo detach vdisk))|diskpart
+.\qemu\qemu-img convert -f vpc filesystem.vhd -O raw filesystem.img
+del filesystem.vhd
