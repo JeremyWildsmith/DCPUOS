42b3963e453107592772f8e60b8bd4e79bdeb443
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2017-06-27 19:48:51 -0700

    Moved stnacs program out of kernel and packaged as seperate process now. Added to common kerneldef.

--- initrd_build.bat
+++ initrd_build.bat
@@ -1,3 +1,4 @@
 xcopy /y initrd_bootstrap\initrd_bootstrap.bin initrd_filesystem\terminal
 xcopy /y SHELL\SHELL.bin initrd_filesystem\txtio
+xcopy /y stnacs\stnacs.bin initrd_filesystem\stnacs
 darkOsRsfs.exe rsfs initrd .\initrd_filesystem
